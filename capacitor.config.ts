c2872135b208b6e474f70c20510dd9001fcd9917
bube009 <207951757+bube009@users.noreply.github.com>
2025-11-29 19:32:00 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: "com.frequency.quantique",
+  appName: "Frequency Quantique",
+  webDir: "dist",
+  bundledWebRuntime: false
+};
+
+export default config;
