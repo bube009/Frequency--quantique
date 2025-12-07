cf03bbad53c171fa96b1581c404a11c4dc4f7327
bube009 <207951757+bube009@users.noreply.github.com>
2025-12-06 21:21:03 -0500

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alexfournier.frequencyquantique',
+  appName: 'Frequency Quantique',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
