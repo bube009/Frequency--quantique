fdf72202f177ea73f49323dd009df20298cf4134
bube009 <207951757+bube009@users.noreply.github.com>
2025-12-09 14:22:01 -0500

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.alexfournierfrequency.quantique',
+  appId: 'com.alexfournier.frequency.quantique',
   appName: 'FrequencyQuantique',
   webDir: 'dist',
   bundledWebRuntime: false
