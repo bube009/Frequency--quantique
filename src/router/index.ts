41523872b98f2b0e664efb9c62de476101dc81a0
bube009 <207951757+bube009@users.noreply.github.com>
2026-01-09 00:20:43 -0500

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,1 +1,11 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router'
+import Home from '../pages/Home.vue'
+import HeartScan from '../pages/HeartScan.vue'
 
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/scan', component: HeartScan }
+  ]
+})
